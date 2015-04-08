1ae5055fafcd973319d089b49fe2c4c8b7e2b573
Jill Kuchman & Bojana Skarich <student@epicodus.com>
2015-04-08 09:57:21 -0700

    Add nickel test and method

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,30 @@
 var CoinCombinations = function(input_value) {
-    return "one penny";
+    var amount_left = input_value  * 100;
+
+
+    var output = "";
+
+    var penny_count = 0;
+    var nickel_count = 0;
+
+    if ((amount_left % 5) !== 0) {
+        penny_count = (amount_left % 5);
+    }
+
+    if (penny_count !== 0){
+        output += penny_count + " p";
+    }
+
+    amount_left = amount_left - penny_count;
+
+    if (amount_left % 5 === 0) {
+        nickel_count = amount_left / 5;
+    }
+
+    if (nickel_count !== 0) {
+        output += nickel_count + " n";
+    }
+
+
+    return output;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,14 @@
 describe('CoinCombinations', function() {
-    it("for one cent value, return one penny", function() {
-        expect(CoinCombinations(.01)).to.equal("one penny");
+    it("for one cent value, return 1 p", function() {
+        expect(CoinCombinations(.01)).to.equal("1 p");
     });
-    
+
+    it("for 1 - 4 cents, return that number of pennies", function() {
+        expect(CoinCombinations(.02)).to.equal("2 p");
+    });
+
+    it("for 5 cents, return 1 nickel", function() {
+        expect(CoinCombinations(.05)).to.equal("1 n");
+    });
 
 });
